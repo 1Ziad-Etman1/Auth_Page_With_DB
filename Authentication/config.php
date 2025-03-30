0945ade7909275ba02bdb1f34626d3b455c4814d
Ziad Etman <115287958+1Ziad-Etman1@users.noreply.github.com>
2025-03-30 12:32:32 +0200

    .

---
+++ Authentication/config.php
@@ -0,0 +1,5 @@
+<?php
+    define('RAPIDAPI_KEY', 'your-rapidapi-key');  
+    define('RAPIDAPI_HOST', 'whatsapp-number-validator3.p.rapidapi.com');
+    define('API_URL', 'https://whatsapp-number-validator3.p.rapidapi.com/WhatsappNumberHasItBulkWithToken');
+?>
