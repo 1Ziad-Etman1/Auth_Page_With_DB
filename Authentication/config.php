b1bddc9a24fc825ad7bda9bf7d62c99ec85ad98d
Tarek Mostafa <102294486+TarekMostafa20@users.noreply.github.com>
2025-05-20 19:44:49 +0300

    old

--- Authentication/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-    define('RAPIDAPI_KEY', '9b737a2573msh5bc3923a2b52103p1c60fejsndf2e96f2ceea');  
-    define('RAPIDAPI_HOST', 'whatsapp-number-validator3.p.rapidapi.com');
-    define('API_URL', 'https://whatsapp-number-validator3.p.rapidapi.com/WhatsappNumberHasItBulkWithToken');
-?>
